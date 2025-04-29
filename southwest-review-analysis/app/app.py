a9d723f5fa8406607bb5b10458b2b6bbac928b4b
Ben Collier <benjamincollier18@icloud.com>
2025-04-29 01:07:50 -0500

    App basics

--- southwest-review-analysis/app/app.py
+++ southwest-review-analysis/app/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+import json
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('home.html')
+
+@app.route('/topics')
+def topics():
+    # Load topics from JSON
+    with open('southwest-review-analysis/data/topic_modeling/topics.json', 'r') as f:
+        topics = json.load(f)
+    return render_template('topics.html', topics=topics)
+
+@app.route('/sentiment')
+def sentiment():
+    df = pd.read_csv('southwest-review-analysis/data/sentiment_analysis/sentiment_southwest_reviews.csv')
+    reviews = df.to_dict(orient='records')  # Converts rows into a list of dictionaries
+    return render_template('sentiment.html', reviews=reviews)
 
 if __name__ == '__main__':
     app.run(debug=True)
